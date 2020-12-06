f9b5386567f23b14fb172d919cbb2ece1f470739
Maksim Svetlakov <MakSim@Mikhails-MacBook-Pro.local>
2020-12-06 16:18:08 -0500

    validation for post completed

--- src/routes.js
+++ src/routes.js
@@ -1,41 +1,70 @@
 import express from 'express'
 import * as help from './helpFns.js'
+//unique id generator from uuid
 import { v4 as uuidv4 } from 'uuid'
 const router = express.Router()
 const entries =[]
 const message = {
-    message: "validation error",
+    message: "validation error", 
     invalid: []
 }
-const items = "hello"
-
-//{error: `missing id or task from request`}fieldName
-
-
+// Bad Request response function
+const badRequest = (input, res) => {
+    res.status(400).json(input)
+}
 const objProps = ["name", "email", "phoneNumber", "content"]
 const objKeys = (req) => Object.keys(req.body)
 const errors = (request) => objProps.filter((properties) => !objKeys(request).includes(properties))
+
+//general request body validation and
+//phone number validation: the only format accepted is 10 digits
+//
 const validateItem = (req, res, next) => {
+    message.invalid = []
     if (objKeys(req).length < 4) {
         errors(req).forEach(element => message.invalid.push(element))
-        return res.status(400).json(message)
-    } 
+        return badRequest(message, res)
+    }
     next()
 }
-//email validation middleware
-const ValidateEmail = (req, res, next) =>{
-    const email = req.body.email
+// strings validation for name, only latin letters accepted
+const validateString = (req, res, next) => {
+    const letters = /^[A-Za-z]+$/;
+    if(!req.body.name.match(letters) || !req.body.name.value == 0){
+        message.invalid.push("name")
+    }
+    next()
+
+}
+// //email validation middleware, generic 
+const validateEmail = (req, res, next) => {
     const mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-    if(!email.match(mailformat)){
-        return res.status(400).json("You have entered an invalid email address!")
+    if(!req.body.email.match(mailformat)){
+        message.invalid.push("email")
+    }
+    next()
+}
+//phone number validation: the only format accepted is 10 digits
+const validatePhone = (req, res, next) => {
+    const phoneno = /^\d{10}$/;
+    if(!req.body.phoneNumber.match(phoneno)){
+        message.invalid.push("phoneNumber")
+    }
+    next()
+}
+//middleware for Bad Request object return based on lenght of object's array entries
+const returnMessage =(req, res, next) => {
+    if (message.invalid.length > 0){
+        return badRequest(message, res)
     }
     next()
 }
 //Route to create an entry when the user submits their contact form:
-router.post('/contact_form/entries', validateItem, ValidateEmail, (req, res) => {
+router.post('/contact_form/entries', validateItem, validateString, validateEmail, validatePhone, returnMessage, (req, res) => {
+        //unique id generator
         req.body.id = uuidv4()
-        entries.push(req.body);
-        return res.status(201).json(req.body.email);
+        entries.push(req.body)
+        return res.status(201).json(req.body)
 })
 
 //Route to create a user:
