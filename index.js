61fbcd118d3cc8618024f8c44def41f0ad308044
Maksim Svetlakov <MakSim@Mikhails-MacBook-Pro.local>
2020-12-07 06:07:30 -0500

    error handling added, JSON parsing, user post

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ const port = 3007;
 
 app.use(express.json());
 app.use('/', routes);
+app.use((req, res, next) => {
+    res.status(404).send({
+    status: 404,
+    message: "not found"
+    })
+    next(error)
+   })
+   
+// error handler middleware
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).send({
+        error: {
+        status: error.status || 500,
+        message: /*error.message ||*/ "Internal Server Error",
+        },
+    });
+});
 
 export default app.listen(port, function () {
   console.log(`API server ready on http://localhost:${port}`);
--- src/routes.js
+++ src/routes.js
@@ -3,25 +3,36 @@ import * as help from './helpFns.js'
 //unique id generator from uuid
 import { v4 as uuidv4 } from 'uuid'
 const router = express.Router()
+const bodyParser = require('body-parser')
 const entries =[]
-const message = {
-    message: "validation error", 
-    invalid: []
-}
+
 // Bad Request response function
 const badRequest = (input, res) => {
     res.status(400).json(input)
 }
-const objProps = ["name", "email", "phoneNumber", "content"]
-const objKeys = (req) => Object.keys(req.body)
-const errors = (request) => objProps.filter((properties) => !objKeys(request).includes(properties))
 
-//general request body validation and
-//phone number validation: the only format accepted is 10 digits
-//
+//general request body validation 
+const objProps = ["name", "email", "phoneNumber", "content"]
+const message = {
+    message: "validation error", 
+    invalid: []
+}
 const validateItem = (req, res, next) => {
     message.invalid = []
+    const objKeys = (req) => Object.keys(req.body)
+    const errors = (request) => objProps.filter((properties) => !objKeys(request).includes(properties))
     if (objKeys(req).length < 4) {
+        errors(req).forEach(element => message.invalid.push(element))
+        return badRequest(message, res)
+    }
+    next()
+}
+//new user general validation
+const validateUser = (req, res, next) => {
+    message.invalid = []
+    const objKeys = (req) => Object.keys(req.body)
+    const errors = (request) => objUserProps.filter((properties) => !objKeys(request).includes(properties))
+    if (objKeys(req).length < 3) {
         errors(req).forEach(element => message.invalid.push(element))
         return badRequest(message, res)
     }
@@ -61,18 +72,22 @@ const returnMessage =(req, res, next) => {
 }
 //Route to create an entry when the user submits their contact form:
 router.post('/contact_form/entries', validateItem, validateString, validateEmail, validatePhone, returnMessage, (req, res) => {
-        //unique id generator
-        req.body.id = uuidv4()
-        entries.push(req.body)
-        return res.status(201).json(req.body)
+    //unique id generator
+    req.body.id = uuidv4()
+    entries.push(req.body)
+    const requestOrganizer = ({ id, name, email, phoneNumber, content }) => ({ id, name, email, phoneNumber, content })
+    return res.status(201).json(requestOrganizer(req.body))
 })
 
 //Route to create a user:
-router.post('/users', /*validateItem,*/ (req, res) => {
-    
-
+const users = []
+const objUserProps = ["name", "password", "email"]
+router.post('/users', validateUser, validateString, validateEmail, returnMessage, (req, res) => {
+    req.body.id = uuidv4()
+    users.push(req.body)
     //items.push(req.body)
-    return res.status(201).send(req.body)
+    const requestFilter = ({ id, name, email }) => ({ id, name, email })
+    return res.status(201).json(requestFilter(req.body))
 })
 
 //Route to log a registered user in to create a JWT (JSON Web Token) token:
